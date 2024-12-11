7d665665b4830823c18b08d995eea14cf84d5b78
Marina Lipatnikova <mlipatnikova2000@mail.ru>
2024-12-11 16:52:58 +0300

    hw

--- 2.1-databases/work_with_database/phones/models.py
+++ 2.1-databases/work_with_database/phones/models.py
@@ -2,5 +2,10 @@ from django.db import models
 
 
 class Phone(models.Model):
-    # TODO: Добавьте требуемые поля
-    pass
+    name = models.CharField(max_length=50)
+    price = models.IntegerField()
+    image = models.URLField()
+    release_date = models.CharField(max_length=50)
+    lte_exists = models.CharField(max_length=50)
+    slug = models.SlugField(unique=True)
+
