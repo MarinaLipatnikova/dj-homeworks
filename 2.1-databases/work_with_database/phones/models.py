0b7676a3ff0ed24c08a19f057239add1bed87dc4
Marina Lipatnikova <mlipatnikova2000@mail.ru>
2024-12-13 16:39:29 +0300

    HW3

--- 2.1-databases/work_with_database/phones/models.py
+++ 2.1-databases/work_with_database/phones/models.py
@@ -1,11 +1,12 @@
 from django.db import models
+from autoslug import AutoSlugField
 
 
 class Phone(models.Model):
     name = models.CharField(max_length=50)
-    price = models.IntegerField()
+    price = models.IntegerField(null=False)
     image = models.URLField()
     release_date = models.CharField(max_length=50)
     lte_exists = models.CharField(max_length=50)
-    slug = models.SlugField(unique=True)
+    slug = AutoSlugField(populate_from='name', max_length=100, unique=True, null=False)
 
